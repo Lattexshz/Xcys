0b1b896f70e3be6679b7624d0675283ca01c9491
Lattexshz <lattex.code@gmail.com>
2023-02-16 07:40:49 +0900

    Able to read toml

--- src/toml.rs
+++ src/toml.rs
@@ -1,4 +1,14 @@
 use crate::color::ColorScheme;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct UDColorScheme {
+    command: Option<String>,
+    sub_command: Option<String>,
+    string: Option<String>,
+    flags: Option<String>
+}
+
 
 pub struct Config {
     scheme: ColorScheme
@@ -6,6 +16,9 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Self {
+
+        let decoded: UDColorScheme = toml::from_str(toml_str).unwrap();
+
         Self {
             scheme: ColorScheme::default()
         }
